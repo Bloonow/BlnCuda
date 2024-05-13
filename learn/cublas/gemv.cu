16caa39d2eb52af954db1ab07c9b1cbc7302511d
Bloonow <excelbloonow@gmail.com>
2024-05-13 09:01:02 +0800

    gemv

--- learn/cublas/gemv.cu
+++ learn/cublas/gemv.cu
@@ -4,10 +4,29 @@
 #include "helper.cu"
 
 int main(int argc, char *argv[]) {
-    const size_t Batch = 4, M = 1024, N = 512;
-    float *h_A = allocHostMemory<float>(Batch * M * N);
-    float *h_x = allocHostMemory<float>(Batch * N);
-    float *h_y = allocHostMemory<float>(Batch * M);
+    const size_t Batch = 4, M = 456, N = 987;
+    const float alpha = 3.14, beta = 2.71;
+    float *h_A = alloc_host_memory<float>(Batch * M * N);
+    float *h_x = alloc_host_memory<float>(Batch * N);
+    float *h_y = alloc_host_memory<float>(Batch * M);
+    float *ret_y = alloc_host_memory<float>(Batch * M);
+    float *d_A = alloc_cuda_memory<float>(Batch * M * N, h_A);
+    float *d_x = alloc_cuda_memory<float>(Batch * N, h_x);
+    float *d_y = alloc_cuda_memory<float>(Batch * M, h_y);
 
+    cublasHandle_t handle;
+    cublasCreate_v2(&handle);
+    // cublasSgemv_v2(handle, CUBLAS_OP_N, M, N, &alpha, d_A, M, d_x, 1, &beta, d_y, 1);  // Batch = 1
+    cublasSgemvStridedBatched(
+        handle, CUBLAS_OP_N, M, N, &alpha, d_A, M, M * N, d_x, 1, N, &beta, d_y, 1, M, Batch
+    );
+    cublasGetVector(Batch * M, sizeof(float), d_y, 1, ret_y, 1);
+    cublasDestroy_v2(handle);
+
+    host_gemv<float>(M, N, COL_MAJOR, h_A, h_x, h_y, alpha, beta, Batch);
+    bool same = check_same<float>(h_y, ret_y, Batch * M, 1e-2);
+    printf(same ? "SAME\n" : "NOT SAME\n");
+
+    free_memory(7, h_A, h_x, h_y, ret_y, d_A, d_x, d_y);
     return 0;
 }
--- learn/cublas/helper.cu
+++ learn/cublas/helper.cu
@@ -1,6 +1,7 @@
 /**
- * All vectors and matrices stored in column-major format.
+ * All vectors and matrices store in column-major format.
  * 🤣 👉 🤡
+ * Matrices can store both in row-major and column-major now.
 */
 
 #include <cuda.h>
@@ -8,7 +9,7 @@
 #include <stdarg.h>
 
 template<typename Ty>
-Ty* allocHostMemory(size_t count, Ty max_init = (Ty)(1)) {
+Ty* alloc_host_memory(size_t count, Ty max_init = (Ty)(1)) {
     static time_t init_time = 0;
     if (init_time == 0) srand(time(&init_time));
     Ty* ptr = reinterpret_cast<Ty*>(malloc(sizeof(Ty) * count));
@@ -19,7 +20,7 @@ Ty* allocHostMemory(size_t count, Ty max_init = (Ty)(1)) {
 }
 
 template<typename Ty>
-Ty* allocCudaMemory(size_t count, Ty *host_ptr = nullptr) {
+Ty* alloc_cuda_memory(size_t count, Ty *host_ptr = nullptr) {
     Ty* ptr;
     cudaMalloc(&ptr, sizeof(Ty) * count);
     if (host_ptr != nullptr) {
@@ -28,7 +29,7 @@ Ty* allocCudaMemory(size_t count, Ty *host_ptr = nullptr) {
     return ptr;
 }
 
-void freeMemory(size_t count, ...) {
+void free_memory(size_t count, ...) {
     va_list vlist;
     va_start(vlist, count);
     for (size_t i = 0; i < count; i++) {
@@ -42,23 +43,36 @@ void freeMemory(size_t count, ...) {
 }
 
 template<typename Ty>
-bool checkSame(Ty *ptr1, Ty *ptr2, size_t num, Ty error = 1e-4) {
+bool check_same(Ty *ptr1, Ty *ptr2, size_t num, Ty error = 1e-4) {
     for (size_t i = 0; i < num; i++) {
         if (abs(ptr1[i] - ptr2[i]) > error) return false;
     }
     return true;
 }
 
+typedef enum {
+    ROW_MAJOR = 0,
+    COL_MAJOR = 1
+} StorageOrder_t;
+
+inline size_t row_index(size_t rid, size_t cid, size_t rows, size_t cols) {
+    return rid * cols + cid;
+}
+
+inline size_t col_index(size_t rid, size_t cid, size_t rows, size_t cols) {
+    return cid * rows + rid;
+}
+
 template<typename Ty>
 void host_gemv(
-    const size_t M, const size_t N, const Ty alpha, const Ty beta,
-    const Ty *A, const Ty *x, Ty *y, const size_t batchCount
+    size_t M, size_t N, StorageOrder_t A_order, Ty *A, Ty *x, Ty *y, Ty alpha, Ty beta, size_t batch_count
 ) {
-    for (size_t bid = 0; bid < batchCount; bid++) {
+    auto A_index = A_order == ROW_MAJOR ? row_index : col_index;
+    for (size_t bid = 0; bid < batch_count; bid++) {
         for (size_t rid = 0; rid < M; rid++) {
             Ty value = 0;
             for (size_t cid = 0; cid < N; cid++) {
-                value += A[bid * M * N + cid * M + rid] * x[bid * N + cid];
+                value += A[bid * M * N + A_index(rid, cid, M, N)] * x[bid * N + cid];
             }
             y[bid * M + rid] = alpha * value + beta * y[bid * M + rid];
         }
